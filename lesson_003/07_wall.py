8f117de7a39feb682bbd5e998ac03abe99ceb7ad
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-05 18:56:05 +0300

    task 07_wall in lesson_003 is completed and delete TODO in task 06_rainbow

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+sd.resolution = (1200, 600)
+for y in range(-20, 801, 50):
+    for x in range(-50, 1201, 100):
+        if y%20 == 0:
+            x += 50
+        left_botton = sd.get_point(x, y)
+        right_botton = sd.get_point(x + 100, y + 50)
+        sd.rectangle(left_botton, right_botton, width=1)
+
+
+sd.pause()
