0a0e15220db68c63e09e0b1424a6dcb52e83b332
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-05 13:22:06 +0300

    task 04_student in lesson_003 is completed

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -1,3 +1,4 @@
+#!/bin/usr/env python3
 # -*- coding: utf-8 -*-
 
 # (цикл while)
@@ -8,7 +9,23 @@
 # чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
 # Формат вывода:
 #   Студенту надо попросить ХХХ.ХХ рублей
+#мне показалось, что интереснее все-таки посчитать разные случаи денежного довольствия, таким
+# образом и усовершенствовал решение
+educational_grant, expenses = 15000, 12000
 
-educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+month = 1
+prihod = educational_grant
+rashod_in_month = expenses
+rashod_all = rashod_in_month
+while month < 10:
+    month +=1
+    rashod_in_month = rashod_in_month * 1.03
+    rashod_all +=rashod_in_month
+    prihod += educational_grant
+    delta = round(prihod - rashod_all, 2)
+if delta < 0:
+    delta = delta * (-1)
+    print ('Студенту необходимо попросить', delta, 'рублей')
+else:
+    print('Студенту хватает денег. Останется', delta, 'рублей')
