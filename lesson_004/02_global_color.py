b72d27c7fe98f8e58f1e98f577cd90037c05f24e
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-09 06:11:36 +0300

    task 02_global_color in lesson_004 is completed

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -14,6 +14,48 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+def drawing(point, angle, length, alpha, color):
+    v = sd.get_vector(start_point=point, angle=alpha+angle, length=length, width=3, )
+    v.draw(color=color)
+    return v.end_point
 
-sd.pause()
+
+def triangle(point, angle, length, color):
+    for alpha in range(0, 241, 120):
+        point = drawing(point, angle, length, alpha, color)
+
+
+def square(point, angle, length, color):
+    for alpha in range(0, 271, 90):
+        point = drawing(point, angle, length, alpha, color)
+
+
+def hexagon(point, angle, length, color):
+    for alpha in range(0, 301, 60):
+        point = drawing(point, angle, length, alpha, color)
+
+
+
+dict_colors_user = {'1':'red', '2':'orange',
+                    '3':'yellow', '4':'green',
+                    '5':'blue', '6':'purple'}
+dict_colors_program = {'1':sd.COLOR_RED, '2':sd.COLOR_ORANGE,
+                       '3':sd.COLOR_YELLOW, '4':sd.COLOR_GREEN,
+                       '5':sd.COLOR_BLUE, '6':sd.COLOR_PURPLE}
+print('возможные цвета:')
+for key, value in dict_colors_user.items():
+    print(key, ':', value)
+s = input('выберете цвет').strip()
+if s in dict_colors_program.keys():
+    sd.resolution = (1200, 600)
+    color = dict_colors_program[s]
+    start_point_triangle = sd.get_point(300, 100)
+    start_point_square = sd.get_point(500, 200)
+    triangle(start_point_triangle, 30, 200, color=color)
+    square(start_point_square, 30, 100, color=color)
+    start_point_hexagon = sd.get_point(800, 300)
+    hexagon(start_point_hexagon, 0, 150, color=color)
+
+    sd.pause()
+else:
+    print('Некорректный ввод')
