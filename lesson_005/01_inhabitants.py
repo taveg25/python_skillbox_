3467fa4b571bbe1993642b5016a977b9f0331656
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-11 07:55:41 +0300

    task 01_inhabitants in lesson_005 is completed

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
-
+import room_1
+import room_2
+print('В комнате room_1 живут:', *room_1.folks)
+print('В комнате room_2 живут:', *room_2.folks)
 
