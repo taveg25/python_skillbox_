8b58359d3ea3654cca020c01bad37a812ac7ff97
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-11 13:08:31 +0300

    task 03_burger in lesson_005 is completed

--- lesson_005/03_burger.py
+++ lesson_005/03_burger.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Создать модуль my_burger. В нем определить функции добавления инградиентов:
@@ -14,5 +15,19 @@
 
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
+# Это фиговое задание, по сути, то же что и чизбургер, будем считать что чизбургер - по моему вкусу))
+import my_burger
+my_burger.bun()
+my_burger.cutlet()
+my_burger.cheese()
+my_burger.cucumber()
+my_burger.tomato()
+my_burger.mayonnaise()
+my_burger.cutlet()
+my_burger.cheese()
+my_burger.cucumber()
+my_burger.tomato()
+my_burger.mayonnaise()
+my_burger.bun()
 
-# TODO здесь ваш код
+
