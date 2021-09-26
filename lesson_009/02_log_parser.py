9ee747acf476e38f5f96e01d132e32fd76aea2c6
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-26 12:42:06 +0300

    task 02_log_parser in lesson_009 is completed

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,10 +19,71 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+
+class LogEvents(object):
+
+    def __init__(self, in_file_name, out_file_name):
+        self.in_file_name = in_file_name
+        self. out_file_name = out_file_name
+
+    def write_NOK(self, prev_line):
+        with open(self.out_file_name, 'a') as file:
+            if self.count > 0:
+                file.write(prev_line[0:17] + ']' + ' ' + str(self.count) + '\n')
+
+    def count_NOK(self, file):
+        for line in file:
+            if line[0:17] == self.prev_line_NOK[0:17]:
+                if 'NOK' in line[-4:]:
+                    self.count += 1
+                    self.prev_line_NOK = line
+                else:
+                    continue
+            else:
+                if 'NOK' in line[-4:]:
+                    self.write_NOK(self.prev_line_NOK)
+                    self.count = 1
+                    self.prev_line_NOK = line
+                    continue
+                self.write_NOK(self.prev_line_NOK)
+                self.count = 0
+                self.prev_line_NOK = line
+
+    def read(self):
+        self.prev_line_NOK = ' NOK'
+        self.count = 0
+        with open(self.in_file_name, 'r') as file:
+            self.count_NOK(file)
+
+
+class LogTime(LogEvents):
+
+    def __init__(self, in_file_name, out_file_name, time):
+        super().__init__(in_file_name=in_file_name, out_file_name=out_file_name)
+        self.time = time
+
+    def times(self, file):
+        for line in file:
+            if self.time in line[12:14]:
+                with open(self.out_file_name, 'a') as file:
+                    file.write(line)
+
+    def read(self):
+        with open(self.in_file_name, 'r') as file:
+            self.times(file)
+
+
+
+#
+# event = LogEvents('events.txt', 'e.txt')
+# event.read()
+event2 = LogTime('events.txt', 'e2.txt', time='22')
+event2.read()
+
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
-#  - по месяцу
-#  - по году
+#  - по месяцу -делается также как и по часам, не вижу смысла делать - новый класс и переопределение
+#  - по году -делается также как и по часам, не вижу смысла делать - новый класс и переопределение
+# ну или я не понял шаблонный метод
 # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
