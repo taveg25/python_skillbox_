3f9b013f1d740a104716b38381f24703a224d954
Anatoliy Ter <tav-rezume@yandex.ru>
2021-09-29 06:34:17 +0300

    task 01_fifth_element in lesson_010 is completed

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
@@ -5,10 +6,16 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
-
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as exc:
+    print(f'Невозможно приобразовать к числу {exc}')
+except IndexError as exc:
+    print(f'Выход за границы списка {exc}')
+except Exception as exc:
+    print(f'Неожиданная ошибка {exc}')
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
