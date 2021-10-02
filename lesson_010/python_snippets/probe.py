ad94edf1494e2beef024abb8f0ed38f361d5388f
Anatoliy Ter <tav-rezume@yandex.ru>
2021-10-02 21:13:57 +0300

    task 02_groundlog_day and 03_registration_log in lesson_010 is completed

--- lesson_010/python_snippets/probe.py
+++ lesson_010/python_snippets/probe.py
@@ -1,11 +1,2 @@
-# s = 'Tanya tanya@ya.ru'
-# a = s.split()
-# name = a[0]
-# email = a[1]
-# age = a[2]
-# print(name)
-# print(email)
-# print(age)
-# print(len(a))
-
-print(type(a[89]))
+print(3%2)
+print((3%2) == True)
