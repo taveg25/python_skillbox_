3b9f30521c8e991364069547794c257020dea8b1
Anatoliy Ter <tav-rezume@yandex.ru>
2021-11-24 06:14:35 +0300

    изменил названия уроков для лучшего поиска

---
+++ lesson_004 - Функции подробнее/lecture_snippets/probe.py
@@ -0,0 +1,12 @@
+class Robot:
+
+    def __init__(self):
+        self.name = 'R2D2'
+
+    def hello(self):
+        print('привет мир!')
+
+
+robot = Robot()
+robot.temperature = 1
+print(robot.temperature)
---
+++ lesson_015 - Библиотеки для работы с данными/probe.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import json
+import re
+from collections import defaultdict
+from datetime import datetime
+from decimal import Decimal, ROUND_HALF_EVEN
+from pprint import pprint
+
+
+mobs = []
+print(bool(mobs))
